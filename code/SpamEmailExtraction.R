c30c6d66c685915b92d28010fe3ca21c118050cd
Charles Siu <chunheisiu@gmail.com>
2018-02-26 14:01:33 -0800

    Remove func

--- code/SpamEmailExtraction.R
+++ code/SpamEmailExtraction.R
@@ -1,20 +1,22 @@
-#First, set the directory to match the local directory for the TRAINING folder
-#on your computer. Below, I give an example on my own personal computer.
+# Set working directory
+setwd("/Users/chunheisiu/Dropbox/Documents/USF/2018_Spring/MATH_373/CaseStudy")
 
-training.directory <- "/Users/chunheisiu/Dropbox/Documents/USF/2018_Spring/MATH_373/CaseStudy/SPAMData/TRAINING"
+# Set directory to folder containing training data
+training.directory <- "/Users/chunheisiu/Dropbox/Documents/USF/2018_Spring/MATH_373/CaseStudy/CaseStudy1/SPAMData/TRAINING"
 
-#Now load training and test files from the SPAM data set
+# Import function
+source("/code/func.R")
+
+# Load training and test files from the SPAM data set
 training.names <- list.files(training.directory)
 
-#next, load each email using the above file.names
+# Load each email using the above file.names
 training.emails <- list()
-setwd(training.directory)
-
 for (i in 1:length(training.names)){
   training.emails[[i]] <- readLines(training.names[i])
 }
 
-#install and load package for easy text analysis
+# Load stringr package for easy text analysis
 library(stringr)
 
 x <- data.frame()
@@ -28,7 +30,6 @@ contains_word <- function(i, word) {
 for (i in 1:length(training.names)){
   x[i, 1] <- contains_word(i, "explosive")
 }
-
 
 #You will use the above type of search to create a matrix of 20 or more binary
 #variables to build into your classifier
--- code/func.R
+++
@@ -1,9 +0,0 @@
-# File containing all functions
-# Include source(func.R) in other files to use these functions
-
-library(stringr)
-
-contains_word <- function(i, word) {
-  word_count <- sum(str_count(training.emails[[i]], word))
-  return (word_count > 0)
-}
