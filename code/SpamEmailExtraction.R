1ad21733c4151d1ac197c82fed800a7ffeef9840
Sahil Jain <jsahil95@gmail.com>
2018-02-28 11:37:38 -0800

    Updated version
    added Naive-Bayes Classifier

--- code/SpamEmailExtraction.R
+++ code/SpamEmailExtraction.R
@@ -135,3 +135,72 @@ labels <- read.table(paste(wd, "/SPAMData/SPAMTrain.label", sep = ""))
 
 # Write df to CSV
 write.csv(x, file = paste(wd, "/classifiers.csv", sep = ""))
+
+
+# Naive-Bayes Classifier 
+nbc <- function(dataTable, y){
+  #Provide check
+  #make sure datatable is a df or matrix
+  #make sure that y is the same length as datatable columns
+
+  
+  #calculating prior probabilities
+  prior <- c()
+  sumOfVariables <- c()
+  total <- nrow(dataTable)
+ 
+  numOfHam <-sum(y)
+  numOfSpam <- total - numOfHam
+  
+  prior[1] <- numOfSpam/total
+  prior[2] <- numOfHam/total
+  
+  #Class conditional probabilities
+  classConditionalsSPAM <- c()
+  classConditionalsHAM <- c()
+
+  for(i in 1:length(dataTable)){
+    variableAndLabelTotal <- 0
+    for(j in total) {
+      if(y[i] == 1 && dataTable[j,i] == 1) {
+        variableAndLabelTotal++
+    append(classConditionalsSPAM, variableAndLabelTotal/total)
+    append(classConditionalsHAM, (total - variableAndLabelTotal)/total)  
+      }
+    }
+  }
+  
+  #Posterior probabilities
+  posteriorSPAM <- c()
+  posteriorHAM <- c()
+  
+  for(i in 1:length(dataTable)){
+    postProbSPAM <- (classConditionalsSPAM[i]*prior[1])
+    posteriorSPAM[i] <- c(postProbSPAM)
+    postProbHAM <- (classConditionalsHAM[i]*prior[2])
+    posteriorHAM[i] <- c(postProbHAM)
+  }
+  
+  #Classify
+  # 0 denoting SPAM, 1 being not SPAM
+  classification <- c()
+  for(i in 1:length(y)){
+    if(posteriorSPAM[i] > 0.5) {
+      classification <- c(classification, 0)
+    }
+    else {
+      classification <- c(classification, 1)
+    }
+  }
+  
+  #Misclassification
+  totalCorrect <- 0
+  for(i in length(classification)){
+    if(classification[i] == y[i]) 
+      totalCorrect++
+  misclassified <- 1-totalCorrect/total
+  }
+  
+  #add to list of outputs
+  return(list(prior, classConditionalsSPAM, classConditionalsHAM, posteriorSPAM, classification, misclassified))
+}
